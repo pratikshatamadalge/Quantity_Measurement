4d05c0b5b048a238f61c5ba7d1e4d40f4c1ed7a2
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-07-09 10:28:44 +0530

    Added:Addition Logic

--- QuantityMeasurement/BuisnessLogic/Addition.cs
+++ QuantityMeasurement/BuisnessLogic/Addition.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace QuantityMeasurement.BuisnessLogic
+﻿namespace QuantityMeasurement.BuisnessLogic
 {
-    class Addition
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Addition
     {
+        public static bool Add(double firstValue, double secondValue, double expected_value)
+        {
+            double result = firstValue + secondValue;
+            if (result == expected_value)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
