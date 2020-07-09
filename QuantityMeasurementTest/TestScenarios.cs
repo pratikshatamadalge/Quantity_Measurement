a5dee8e40bbe5f29cb2ae58bc2f3374dceebfc6c
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-07-09 08:06:35 +0530

    Added:Test cases for feet comparison

--- QuantityMeasurementTest/TestScenarios.cs
+++ QuantityMeasurementTest/TestScenarios.cs
@@ -2,10 +2,10 @@ namespace QuantityMeasurementTest
 {
     using NUnit.Framework;
     using QuantityMeasurement.BuisnessLogic;
+    using QuantityMeasurement.Lenghts;
     using QuantityMeasurement.MetricsUnit;
     using static QuantityMeasurement.BuisnessLogic.Conversion;
     using static QuantityMeasurement.MetricsUnit.Units;
-    using QuantityMeasurement.Lenghts;
 
     public class Tests
     {
@@ -30,6 +30,37 @@ namespace QuantityMeasurementTest
            Feet first_value = new Feet(0.0);
            Feet second_value = new Feet(0.0);
            Assert.IsTrue(first_value.ValueInFeet.Equals(second_value.ValueInFeet));
+        }
+
+        [Test]
+        public void ZeroFeet_And_NullValue_should_Return_False()
+        {
+            Feet feet = new Feet();
+            Assert.IsFalse(feet.Equals(null));
+        }
+
+        [Test]
+        public void ZeroFeet_And_ZeroFeet_Should_Return_True()
+        {
+            this.Result = UnitConvertor<Length>(0, Length.FEET) == UnitConvertor<Length>(0, Length.FEET);
+            Assert.IsTrue(this.Result);
+        }
+
+        [Test]
+        public void  ReferenceCheck_Test_For_FEET()
+        {
+            Feet first_value = new Feet();
+            Feet second_value = new Feet();
+            Assert.IsFalse(first_value.Equals(second_value));
+        }
+
+        [Test]
+        public void ImproperType_object_should_Return_False()
+        {
+            Feet first_value=new Feet();
+            object second_value = new Feet();
+
+            Assert.IsFalse(first_value.Equals(second_value));
         }
     }
 }
